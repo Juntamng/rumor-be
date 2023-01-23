1e2db5d7b7e9c97554130b3bc954620cd6bd8341
Mike Ng <junta.m.ng@gmail.com>
2023-01-23 15:34:33 -0500

    try fixed cros in header

--- app.ts
+++ app.ts
@@ -40,7 +40,7 @@ interface itemData {
 
 app.use(express.json());
 
-const allowedOrigins = ['http://localhost:3000', "https://rumor-fe.vercel.app"];
+const allowedOrigins = ['*'];
 const options: cors.CorsOptions = {
   origin: allowedOrigins
 };
@@ -48,6 +48,16 @@ const options: cors.CorsOptions = {
 app.use(cors(options));
 
 app.get('/', async (req: Request, res: Response) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  
   const client = new Client({ apiKey: pexelsKey })
 
   const page:number = (req.query.page) ? parseInt(req.query.page as string) : 1
